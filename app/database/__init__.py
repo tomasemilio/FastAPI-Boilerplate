0e7bbc14d70197b3df2ac0741db12e25717df7e6
tomasemilio <tsilvaebensperger@gmail.com>
2024-02-22 13:22:41 -0300

    Github workflows

--- app/database/__init__.py
+++ app/database/__init__.py
@@ -2,4 +2,13 @@ from sqlmodel import create_engine
 
 from app.config import config
 
-engine = create_engine(url=config.DB_URL, pool_pre_ping=True)
+if config.DB_URL:
+    connect_args = (
+        {"check_same_thread": False} if config.DB_URL.startswith("sqlite") else {}
+    )
+    engine = create_engine(
+        url=config.DB_URL,
+        pool_pre_ping=True,
+        connect_args=connect_args,
+        pool_size=10,
+    )
