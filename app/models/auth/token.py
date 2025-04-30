9a4c407fa5c8fca8127b24d1fbf9084f7170412e
tomasemilio <tsilvaebensperger@gmail.com>
2025-04-30 12:25:28 -0400

    Token class, avoid UUID.hex

--- app/models/auth/token.py
+++ app/models/auth/token.py
@@ -1,8 +1,7 @@
 from datetime import UTC, datetime, timedelta
-from typing import Annotated
 from uuid import UUID
 
-from pydantic import AfterValidator, BaseModel, Field, computed_field
+from pydantic import BaseModel, Field, computed_field
 
 from app.config import config
 from app.functions.exceptions import forbidden
@@ -12,17 +11,21 @@ from app.models.auth.schemas import TokenDecode, TokenEncode
 
 
 class Token(BaseModel):
-    id: Annotated[UUID, AfterValidator(lambda x: x.hex)]
+    id: UUID
     scope: list[Role] = [Role.USER]
     expires_in: int = config.TOKEN_EXPIRE_SECONDS
     iat: datetime = Field(default_factory=lambda: datetime.now(UTC))
 
     @computed_field
+    @property
     def exp(self) -> datetime:
         return self.iat + timedelta(seconds=self.expires_in)
 
     def encode(self) -> TokenEncode:
-        token = encode(**self.model_dump())
+        data = self.model_dump(mode="json", exclude={"iat", "exp"}) | dict(
+            iat=self.iat.timestamp(), exp=self.exp.timestamp()
+        )
+        token = encode(**data)
         return TokenEncode(
             access_token=token, expires_in=self.expires_in, scope=self.scope
         )
